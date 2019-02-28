cd3ddf864e20598aa80ccb53880b4ba7d404fce3
rlagy <rlagy@DESKTOP-SHPDQRT>
2019-03-01 02:17:26 +0900

    Method Array Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,35 +3,29 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	public static void main(String[] args) {
+	private static final int[] Max_days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static int getMaxDaysOfMonth(int month) {
+		return Max_days[month-1];
+	}
+	
+	public static void SamepleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
+	}
+	public static void main(String[] args) {
 		
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		int month = scanner.nextInt();
-		int result;
-		if(month <=7) {
-			if(month%2==1) {
-				result = 31;
-			}else if(month != 2 && month%2==0) {
-				result = 30;
-			}else {
-				result = 28;
-			}
-		}else {
-			if(month%2==1) {
-				result = 30;
-			}else {
-				result = 31;
-			}
-		}
-		System.out.printf("%d월은 %d일까지 있습니다.", month, result);
+		
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
+		scanner.close();
 		
 	}
 }
