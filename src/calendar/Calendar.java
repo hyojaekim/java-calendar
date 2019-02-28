c29b0b38c70650746d59895b18e666c0f16222c6
rlagy <rlagy@DESKTOP-SHPDQRT>
2019-02-28 23:07:27 +0900

    Update Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
 		System.out.println("일 월 화 수 목 금 토");
@@ -8,6 +10,28 @@ public class Calendar {
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-
+		
+		
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		int result;
+		if(month <=7) {
+			if(month%2==1) {
+				result = 31;
+			}else if(month != 2 && month%2==0) {
+				result = 30;
+			}else {
+				result = 28;
+			}
+		}else {
+			if(month%2==1) {
+				result = 30;
+			}else {
+				result = 31;
+			}
+		}
+		System.out.printf("%d월은 %d일까지 있습니다.", month, result);
+		
 	}
 }
